c63b1a5bd155f50a273989d6473dea40402d2012
Talisson Oliveira <talissonlux@outlook.com>
2022-09-13 22:46:43 -0300

    feat: adds the fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,23 @@
 import { StatusBar } from 'react-native'
 import { Background } from './src/components/Background'
+import { Home } from './src/screens/Home'
+import { Loading } from './src/components/Loading'
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  })
+
   return (
     <Background>
       <StatusBar
@@ -9,6 +25,8 @@ export default function App() {
         barStyle="light-content"
         backgroundColor="transparent"
       />
+
+      {fontsLoaded ? <Home /> : <Loading />}
     </Background>
   )
 }
