1f7f6c17ba76f6818dafce4831faf215d9c7b9b9
Talisson Oliveira <talissonlux@outlook.com>
2022-09-14 01:14:15 -0300

    feat: creates the home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,35 @@
-import { View } from 'react-native'
+import { FlatList, Image, View } from 'react-native'
+import { GAMES } from '../../utils/games'
+import { Heading } from '../../components/Heading'
+import { GameCard } from '../../components/GameCard'
+
+import logoImg from '../../assets/logo-nlw-esports.png'
 
 import { styles } from './styles'
 
 export function Home() {
   return (
     <View style={styles.container}>
+      <Image
+        source={logoImg}
+        style={styles.logo}
+      />
 
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <FlatList
+        data={GAMES}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <GameCard data={item} />
+        )}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.contentList}
+        horizontal
+      />
     </View>
   )
 }
