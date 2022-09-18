a600dbc7f28a05c536075ef6ece8c1a6e5ae3fda
Talisson Oliveira <talissonlux@outlook.com>
2022-09-18 03:38:59 -0300

    feat: adds push notifications with expo

--- App.tsx
+++ App.tsx
@@ -1,4 +1,8 @@
+import * as Notifications from 'expo-notifications'
+import { useEffect, useRef } from 'react'
 import { StatusBar } from 'react-native'
+import { Subscription } from 'expo-modules-core'
+import { getPushNotificationToken } from './src/services/getPushNotificationToken'
 import { Background } from './src/components/Background'
 import { Loading } from './src/components/Loading'
 import { Routes } from './src/routes'
@@ -10,6 +14,8 @@ import {
   Inter_900Black
 } from '@expo-google-fonts/inter'
 
+import './src/services/notificationConfigs'
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
@@ -17,6 +23,30 @@ export default function App() {
     Inter_700Bold,
     Inter_900Black
   })
+
+  const getNotificationListener = useRef<Subscription>()
+  const responseNotificationListener = useRef<Subscription>()
+
+  useEffect(() => {
+    getPushNotificationToken()
+  }, [])
+
+  useEffect(() => {
+    getNotificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification)
+    })
+
+    responseNotificationListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response)
+    })
+
+    return () => {
+      if (getNotificationListener.current && responseNotificationListener.current) {
+        Notifications.removeNotificationSubscription(getNotificationListener.current)
+        Notifications.removeNotificationSubscription(responseNotificationListener.current)
+      }
+    }
+  }, [])
 
   return (
     <Background>
